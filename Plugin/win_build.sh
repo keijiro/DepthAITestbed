fbb748221de4de6d88529867da3d8446b2c8751e
goloskokovic <goloskokovic@gmail.com>
2021-12-29 07:19:11 +0100

    allow spaces in path for Hunter

--- Plugin/win_build.sh
+++ Plugin/win_build.sh
@@ -3,7 +3,7 @@
 TARGET="DepthCamera"
 PACKAGE_DIR="../jp.keijiro.depthai.depthcamera/Runtime"
 
-cmake -D'depthai_DIR=../../depthai-core/build' -S. -Bbuild
+cmake -D'depthai_DIR=../../depthai-core/build' -S. -Bbuild -D'HUNTER_ALLOW_SPACES_IN_PATH=ON'
 cmake --build build --parallel
 
 rm ${PACKAGE_DIR}/*.dll
